68b3d891e0e3bcd4de00070362209bf24fa2f23d
Jefferson Balivo <jefferson.balivo@onfriday.com.br>
2015-11-03 09:39:16 -0200

    Projeto exemplo completo

---
+++ SiteIdentitySolution/src/SiteIdentity.Data/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.Framework.Configuration;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Dnx.Runtime;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Data.Entity;
+
+namespace SiteIdentity.Data
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(appEnv.ApplicationBasePath)
+                .AddJsonFile("appsettings.json");
+
+            this.Configuration = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddEntityFramework()
+                .AddSqlServer()
+                .AddDbContext<SiteIdentityDbContext>(o => o.UseSqlServer(this.Configuration["Data:DefaultConnection:ConnectionString"]));
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+
+        }
+    }
+}
